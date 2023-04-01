e4ca0b83f0588315df872b683aefb275742d727b
Rudis159 <77154023+Rudis159@users.noreply.github.com>
2023-04-01 17:40:34 +0300

    Created db
    Created db minimaldb

---
+++ StudentAPI/StudentAPI/DataContext.cs
@@ -0,0 +1,14 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace StudentAPI
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students => Set<Student>();
+    }
+}
